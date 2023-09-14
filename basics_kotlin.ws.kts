a8f082b85507c845b091e17c3220dc8e0ff52879
Jirat Chokdeeanan <jirat.cho@gmail.com>
2023-09-14 19:02:41 +0700

    added worksheet

---
+++ basics_kotlin.ws.kts
@@ -0,0 +1,43 @@
+// Hello World!
+fun main () {
+    println("Hello World")
+}
+
+
+// Basic Type
+val int: Int = 1
+val long: Long = 1L
+
+print(int)
+print(long)
+
+// String templates
+
+// Collection
+
+// Variables
+
+// Class vs Data Class
+
+// Functions
+
+// If
+
+// When
+
+// Operator
+
+// Loops
+
+// Ranges
+
+// Null safety
+
+// Elvis
+
+// Safe Call
+
+// Unsafe Calls
+
+// Scope function
+
