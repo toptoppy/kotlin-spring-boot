b78b95e8c973160ccf0367fdcb33cdcd1c06f72b
Jirat Chokdeeanan <jirat.cho@gmail.com>
2023-09-14 19:04:50 +0700

    added worksheet

--- basics_kotlin.ws.kts
+++ basics_kotlin.ws.kts
@@ -7,9 +7,13 @@ fun main () {
 // Basic Type
 val int: Int = 1
 val long: Long = 1L
+val float: Float = 1.0
+val double: Double = 20.00
 
 print(int)
 print(long)
+print(float)
+print(double)
 
 // String templates
 
